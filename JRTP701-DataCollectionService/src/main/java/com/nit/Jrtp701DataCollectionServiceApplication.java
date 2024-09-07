d9a4f3216861bb71464cdf40f12c82dd78b265ef
mkumar-svg <mkumar800534@gmail.com>
2024-09-07 14:09:23 +0530

    Data Collection Service

--- JRTP701-DataCollectionService/src/main/java/com/nit/Jrtp701DataCollectionServiceApplication.java
+++ JRTP701-DataCollectionService/src/main/java/com/nit/Jrtp701DataCollectionServiceApplication.java
@@ -2,8 +2,10 @@ package com.nit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Jrtp701DataCollectionServiceApplication {
 
 	public static void main(String[] args) {
